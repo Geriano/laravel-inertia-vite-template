12f7a74e10d56dd5e0ee8c75a8023ffc3030d012
Geriano <gerznewbie@gmail.com>
2022-07-15 13:08:01 +0700

    adding username column to users table and create user seeder

--- database/seeders/InitialSeeder.php
+++ database/seeders/InitialSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class InitialSeeder extends Seeder
 {
@@ -14,6 +16,24 @@ class InitialSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $su = User::create([
+            'name' => 'root',
+            'username' => 'su',
+            'email' => 'root@local',
+            'password' => $password = Hash::make('password'),
+        ]);
+
+        $su->email_verified_at = now();
+        $su->save();
+
+        $user = User::create([
+            'name' => 'user',
+            'username' => 'user',
+            'email' => 'user',
+            'password' => $password,
+        ]);
+
+        $user->email_verified_at = now();
+        $user->save();
     }
 }
