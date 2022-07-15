c7122650785807bf7118d14fa1e6f45193a47af8
Geriano <gerznewbie@gmail.com>
2022-07-16 02:29:09 +0700

    create permission and role model

---
+++ app/Models/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Permission\Models\Permission as Model;
+
+class Permission extends Model
+{
+  // 
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,16 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasPermissions;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
+    use HasPermissions;
+    use HasRoles;
     use Notifiable;
     use TwoFactorAuthenticatable;
 
