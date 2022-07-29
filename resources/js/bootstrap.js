0d87f5dd97621461384e90f7422931e511b36aa8
Geriano <gerznewbie@gmail.com>
2022-07-29 13:33:51 +0700

    configuring broadcast with laravel-websockets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,17 +18,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: import.meta.env.VITE_PUSHER_APP_KEY,
+  wsHost: import.meta.env.VITE_PUSHER_APP_HOST || `localhost`,
+  wsPort: import.meta.env.VITE_PUSHER_APP_PORT || 6001,
+  wssPort: 443,
+  forceTLS: (import.meta.env.VITE_PUSHER_SCHEME || 'http') === 'https',
+  enabledTransports: ['ws', 'wss'],
+});
