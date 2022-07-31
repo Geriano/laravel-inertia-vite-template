817b4fc7fb5666eb0a4d6386d13ccd752425c39f
Geriano <gerznewbie@gmail.com>
2022-07-31 11:53:09 +0700

    change close button color to red

--- resources/js/Components/Button/Close.vue
+++ resources/js/Components/Button/Close.vue
@@ -3,5 +3,5 @@ import Icon from '@/Components/Icon.vue'
 </script>
 
 <template>
-  <Icon name="times" class="px-2 py-1 bg-gray-300 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:active:bg-gray-800 border border-transparent dark:active:border-gray-600 rounded-md transition-all duration-300 cursor-pointer" />
+  <Icon name="times" class="px-2 py-1 bg-red-500 hover:bg-red-600 dark:text-gray-700 rounded-md transition-all duration-300 cursor-pointer" />
 </template>
